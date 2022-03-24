8cff526e8507be7d2cbf5b28544d561f06a3bcfa
ayer <33113966+abdiyer@users.noreply.github.com>
2022-03-24 18:00:13 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// Import express into our project
+const express = require("express");
+
+// Creating an instance of express function
+const app = express();
+
+// The port we want our project to run on
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+
+// Render the index.html when the user visit our project port
+app.get("/", (req, res) => {
+  res.sendFile("/index.html");
+});
+
+// Express allows us to listen to the PORT and trigger a console.log() when you visit the port
+app.listen(PORT, () => {
+  console.log(`Server is 🏃‍♂️ on port ${PORT}`);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
